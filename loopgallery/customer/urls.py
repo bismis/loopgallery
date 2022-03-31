384da8d328324566732d99fd25cc88c561997b7c
bismi <bismisnndd@gmail.com>
2022-03-31 13:23:53 +0530

    user created and completed registration

--- loopgallery/customer/urls.py
+++ loopgallery/customer/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('login/',views.login1,name='login page'),
     path('register/',views.register,name='register page'),
     path('login/Signup/',views.signup,name='sign in user'),
-    path('logout',views.logout,name='logout page')
+    path('logout',views.logout,name='logout page'),
+    path('register/regin',views.regin,name='register in')
 ]
--- loopgallery/customer/views.py
+++ loopgallery/customer/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render,redirect
-from django.contrib.auth.models import auth
+from django.contrib.auth.models import auth, User
 
 
 # Create your views here.
@@ -24,3 +24,27 @@ def signup(request):
 def logout(request):
     auth.logout(request)
     return redirect('/')
+
+def regin(request):
+    firstname=request.POST['fname']
+    lastname=request.POST['lname']
+    username=request.POST['user']
+    Email=request.POST['email']
+    Password=request.POST['pswf']
+    Re_password=request.POST['psws']
+
+    if Password==Re_password:
+        if User.objects.filter(username=username).exists():
+            return render(request,'register.html',{'emsg':'username is already taken'})
+        elif User.objects.filter(email=Email).exists():
+            return render(request,'register.html',{'emsg':'Email is already taken'})
+
+        else:
+            user=User.objects.create_user(username=username,first_name=firstname,last_name=lastname,email=Email,password=Password)
+            user.save();
+            print('user created')
+            auth.login(request,user)
+            return redirect('/')
+    else:
+        return render(request,'register.html',{'emsg':'password is not match'})
+
